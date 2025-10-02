bf9084550795476c3fe69510c8f4793fa0915eb5
seirenmong <seirenmong@gmail.com>
2025-10-02 18:11:53 +0900

    add DAO(code : asdds)
    dao에 코드 추가 연습

--- MyGuestbook/src/main/java/com/team/dao/GuestBookDAO.java
+++ MyGuestbook/src/main/java/com/team/dao/GuestBookDAO.java
@@ -13,7 +13,7 @@ import com.team.dto.GuestBookDTO;
 
 public class GuestBookDAO {
     private static SqlSessionFactory sqlSessionFactory;
-
+asdds
     static {
         try {
             String resource = "mybatis-config.xml";
