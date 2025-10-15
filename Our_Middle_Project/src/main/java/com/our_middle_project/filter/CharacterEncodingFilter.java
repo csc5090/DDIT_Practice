6d973af4c70c8b77fc394a65de2eb630760b54c5
csc5090 <tkdcjf5090@gmail.com>
2025-10-15 10:52:53 +0900

    Start Set
    1.패키지 4개 생성.
    그 후 필터 패키지와 필터 클래스 생성.
    자세한 역할은 안에 적어두었음(주석으로)
    
    2.마이바티스 기본 셋팅
    교수님이 말씀해주신대로 셋팅해 둠.
    
    3.jar 파일 5가지(마이바티스,jdbc,jstl,el,롬복) 셋팅해둠.
    
    4.web.xml파일 업데이트(2.4->3.0.x에 맞게)

---
+++ Our_Middle_Project/src/main/java/com/our_middle_project/filter/CharacterEncodingFilter.java
@@ -0,0 +1,27 @@
+package com.our_middle_project.filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+
+
+// @WebFilter("/*") : 모든 주소 요청을 가로채고, 먼저 인코딩 설정을 검사해주겠다는 어노테이션.
+// 이 덕분에 우리가 서블릿, jsp를 만들 때 마다 관련 셋팅 코딩을 하지 않아도 됨.
+// 모든 요청, 응답을 UTF-8로 통일하겠다는 뜻.
+
+@WebFilter("/*")
+public class CharacterEncodingFilter implements Filter {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
+    		throws IOException, ServletException {
+        
+        
+        request.setCharacterEncoding("UTF-8");
+
+        chain.doFilter(request, response);
+    }
+}
---
+++ Our_Middle_Project/src/main/webapp/index.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+<h1>Our_Middle_Project</h1>
+<p>이 페이지가 보인다면, 서버 설정에 문제가 없다는 것.</p>
+
+</body>
+</html>
