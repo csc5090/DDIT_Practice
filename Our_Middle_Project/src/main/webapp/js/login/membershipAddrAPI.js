e0d6a6249fe9ece6ffd29162843ec10cd9183716
SH-Programer <113085133+SH-Programer@users.noreply.github.com>
2025-10-28 18:03:04 +0900

    회원가입 수정

--- Our_Middle_Project/src/main/webapp/js/login/membershipAddrAPI.js
+++ Our_Middle_Project/src/main/webapp/js/login/membershipAddrAPI.js
@@ -1,4 +1,4 @@
-function sample6_execDaumPostcode() {
+function addrSearchAPI() {
     new daum.Postcode({
         oncomplete: function(data) {
             // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
@@ -31,17 +31,17 @@ function sample6_execDaumPostcode() {
                     extraAddr = ' (' + extraAddr + ')';
                 }
                 // 조합된 참고항목을 해당 필드에 넣는다.
-                document.getElementById("sample6_extraAddress").value = extraAddr;
+                document.getElementById("user_exAddr").value = extraAddr;
             
             } else {
-                document.getElementById("sample6_extraAddress").value = '';
+                document.getElementById("user_exAddr").value = '';
             }
 
             // 우편번호와 주소 정보를 해당 필드에 넣는다.
-            document.getElementById('sample6_postcode').value = data.zonecode;
-            document.getElementById("sample6_address").value = addr;
+            document.getElementById("user_postCode").value = data.zonecode;
+            document.getElementById("user_addres").value = addr;
             // 커서를 상세주소 필드로 이동한다.
-            document.getElementById("sample6_detailAddress").focus();
+            document.getElementById("user_detailAddr").focus();
         }
     }).open();
 }
