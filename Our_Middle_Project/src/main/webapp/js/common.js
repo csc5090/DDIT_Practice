127c193a9e452c92b35926503831394f1ac87ae2
csc5090 <tkdcjf5090@gmail.com>
2025-10-29 17:01:40 +0900

    common.js update
    오타수정

--- Our_Middle_Project/src/main/webapp/js/common.js
+++ Our_Middle_Project/src/main/webapp/js/common.js
@@ -1,5 +1,5 @@
 const PROTOCOL_HOST = "http://localhost:"
-const PROT = "8095"
+const PORT = "8095"
 const P_NAME = "/Our_Middle_Project"
 
-const BASE_URL = PROTOCOL_HOST + PROT + P_NAME;
+const BASE_URL = PROTOCOL_HOST + PORT + P_NAME;
